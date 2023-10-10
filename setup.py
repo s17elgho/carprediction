2fefe5304bf0c4012f364770989d4b0f9c879da4
unknown <salma011998@gmail.com>
2023-10-10 15:04:20 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    with open(file_path) as f :
+        requirements = f.readlines()
+    requirements = [req.replace('\n','') for req in requirements]
+    if HYPHEN_E_DOT in requirements : 
+        requirements.remove(HYPHEN_E_DOT)
+    return(requirements)
+
+setup(
+name = "testproject",
+version = "0.0.1",
+author = "SalmaELGH",
+author_email ="salma011998@gmail.com",
+packages = find_packages(),
+install_requires = get_requirements("requirements.txt")
+
+)
